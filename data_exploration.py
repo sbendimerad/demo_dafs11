bf5481e12ba0abc0e46324f3264b5541dbd58292
sbendimerad <sabrine.bendimerad1@gmail.com>
2025-02-11 21:41:20 +0100

    Added experimental feature

--- data_exploration.py
+++ data_exploration.py
@@ -15,19 +15,4 @@ print(df.head())
 
 print(df.describe())
 
-## First time git setup
-# 1. Create a git repository on github
-# 2. Create the repository on local machine 
-# 3. echo "# dafs11 demo" >> README.md
-# 4. git init (initialize the git repository on local machine in the reight directory)
-# 5. git add README.md
-# 6. git commit -m "first commit"
-# 7. git branch -M main
-# 8. git remote add origin "url of the repository"
-# 9. git push -u origin main
-
-## Pushing the changes to the repository
-# git status
-# git add data_exploration.py
-# git commit -m "Add data exploration script"
-# git push origin main
+print(df.info())
